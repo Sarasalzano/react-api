01933601f786b8f9807ac536bb0343a483f4c5b8
Sara Salzano <sarasalzano95@gmail.com>
2025-10-02 14:56:36 +0200

    esegui milestone 1

--- src/components/ActorsList.jsx
+++ src/components/ActorsList.jsx
@@ -4,16 +4,25 @@ import { useState, useEffect } from "react";
 export default function ActorsList(){
 //creazione stato degli attori con array vuoto poiché lista verrò aggiornata tramite api
 const [Actors, setActors] = useState([]);
+
 //funzione per richiamare dati
 function fetchActors() {
     //axios fa richiesta ad api
     axios.get("https://lanciweb.github.io/demo/api/actors/")
     //quando la richiesta funziona allora verranno verranno estrapolati gli oggetti contenenti dati utili
-    .then((res) => setActors(res.data))
+    .then((res) => {
+    setActors(res.data);
+    //stampa in console della lista attori da API (milestone 1)
+    console.log(res.data);
+    })
     //gestione errori. In caso di errori, l'errore verrà stampato
-    .catch(error => console.log(error))
-    
-}
+    .catch(error => console.log(error));
+};
+
+//useEffect: esegue fetchActors una sola volta
+useEffect(() => {
+    fetchActors()
+}, [])
 
 
     return(
